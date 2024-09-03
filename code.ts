d351732c04201c5589d5ecc1a1dbf2c0221c73e2
Jcavenaghi <joaquincavenaghi@gmail.com>
2024-09-03 18:38:32 -0300

    feat: v1 of insert iframe with sound in figma

--- code.ts
+++ code.ts
@@ -16,7 +16,7 @@ figma.ui.onmessage = async msg => {
     if (access_token) {
       registered = true;
     }
-    figma.ui.postMessage({ type: 'result-token', value: registered});
+    figma.ui.postMessage({ type: 'result-token', value: registered });
 
   } else if (msg.type === 'initialize-tokens') {
     const access_token = await figma.clientStorage.getAsync('access_token');
@@ -25,7 +25,7 @@ figma.ui.onmessage = async msg => {
     if (access_token && refresh_token) {
       registered = true;
     }
-    figma.ui.postMessage({ type: 'initialize-tokens-result', value: registered, tokens: { access_token, refresh_token} });
+    figma.ui.postMessage({ type: 'initialize-tokens-result', value: registered, tokens: { access_token, refresh_token } });
 
   } else if (msg.type === 'save-token') {
     await figma.clientStorage.setAsync('access_token', msg.token.access_token);
@@ -36,26 +36,41 @@ figma.ui.onmessage = async msg => {
     if (keys.length > 0) {
       figma.ui.postMessage({ type: 'keys', keys });
     } else {
-      figma.ui.postMessage({ type: 'init'});
+      figma.ui.postMessage({ type: 'init' });
     }
 
   } else if (msg.type === 'get-sound') {
     const data = await figma.clientStorage.getAsync(msg.key);
-    const url = await figma.clientStorage.getAsync(msg.key+"-url");
-    const image = await figma.clientStorage.getAsync(msg.key+"-image");
+    const url = await figma.clientStorage.getAsync(msg.key + "-url");
+    const image = await figma.clientStorage.getAsync(msg.key + "-image");
     const external = url && image;
     figma.ui.postMessage({ type: 'sound', key: msg.key, value: data, url, image, external });
 
   } else if (msg.type === 'sound-uploaded') {
     await figma.clientStorage.setAsync(msg.sound.key, msg.sound.metaphors);
-    await figma.clientStorage.setAsync(msg.sound.key+"-url", msg.sound.url);
-    await figma.clientStorage.setAsync(msg.sound.key+"-image", msg.sound.image);
+    await figma.clientStorage.setAsync(msg.sound.key + "-url", msg.sound.url);
+    await figma.clientStorage.setAsync(msg.sound.key + "-image", msg.sound.image);
 
   } else if (msg.type === 'cancel') {
     figma.closePlugin();
+  } else if (msg.type === 'add-to-figma') {
+    const iframe = figma.createFrame();
+    iframe.resize(400, 400);
+    iframe.x = 100;
+    iframe.y = 100;
+    iframe.name = 'Sound';
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="400" height="400">
+      <rect width="100%" height="100%" fill="blue" />
+      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="white" font-size="24">window.navigator.vibrate([200, 100, 200]);</text>
+    </svg>`;
+
+    const html = figma.createNodeFromSvg(svg);
+    html.resize(400, 400);
+    iframe.appendChild(html);
+    figma.currentPage.appendChild(iframe);
   }
 };
 
-async function updateSound(sound: {name: string, metaphors: string}) {
+async function updateSound(sound: { name: string, metaphors: string }) {
   await figma.clientStorage.setAsync(sound.name, sound.metaphors);
 }
